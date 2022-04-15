07c451a8f9f9d05dcbd4c8e6afe9f63acd49fd8b
Collin Lowing <collin.lowing@protonmail.com>
2022-04-14 20:59:47 -0500

    finished python generation

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import parse
 import fsa
 
 fsa_filename = 'fsa.txt'
-output_file = 'part2.lisp'
-string_filename = ''
+output_filename = 'part2.lisp'
+string_filename = 'theString.txt'
 parser = parse.Parser(fsa_filename)
 fsa = fsa.FSA()
 tokens = []
@@ -20,10 +20,9 @@ states = []
 state_nodes = []
 transition_nodes = []
 input_string = ''
-lisp_stream = ''
+
 
 def parse_fsa():
-
     global tokens
     tokens = parser.read_tokens()
 
@@ -56,14 +55,7 @@ def check_fsa():
 
 
 def read_string():
-    argc = len(sys.argv)
-
-    if argc < 2:
-        print('error: no commandline arguments')
-        quit()
-
-    input_filename = sys.argv[1]
-    file = open(input_filename, 'r')
+    file = open(string_filename, 'r')
     global input_string
     input_string = file.readline()
 
@@ -74,7 +66,6 @@ def read_string():
 
 
 def load_fsa():
-
     for transition in transitions:
         if transition[0] not in states:
             states.append(transition[0])
@@ -106,10 +97,12 @@ def load_fsa():
     # # print transitions for testing
     # for transition_node in transition_nodes:
     #     transition_node.print()
+
+
 def generate_state(state):
     stream = ''
     state_num = state.get_number()
-    state_transitions = fsa.FSA.get_possible_transitions(state, transition_nodes)
+    state_transitions = fsa.get_possible_transitions(state, transition_nodes)
 
     stream += '(defun state' + state_num + '(strlist)\n'
     stream += '\t(if (= (length strlist) 0)\n'
@@ -117,20 +110,21 @@ def generate_state(state):
         stream += '\t\t(return-from state' + state_num + ' 1))\n'
     else:
         stream += '\t\t(return-from state' + state_num + ' 0))\n'
-    stream += '\t(let ((sublist strlist) (n 0))'
-    stream += '\t\t(dolist (L strlist)\n'
+    stream += '\t(let ((sublist strlist) (n 0))\n'
+    stream += '\t\t(dolist (C strlist)\n'
     stream += '\t\t\t(cond\n'
     for s_transition in state_transitions:
         transition_char = s_transition.get_alpha()
         next_state = s_transition.get_end_state()
         if next_state.get_number() == state.get_number():
-            stream += '\t\t\t\t((STRING-EQUAL L "' + transition_char + '") (setf n (+ n 1)))\n'
+            stream += '\t\t\t\t((STRING-EQUAL C "' + transition_char + '") (setf n (+ n 1)))\n'
         else:
-            stream += '\t\t\t\t((STRING-EQUAL L "' + transition_char + '") (return-from state' + state_num + ' (state' + next_state.get_number() + ' (subseq sublist (+ n 1) (length sublist)))))\n'
+            stream += '\t\t\t\t((STRING-EQUAL C "' + transition_char + '") (return-from state' + state_num + ' (state' + next_state.get_number() + ' (subseq sublist (+ n 1) (length sublist)))))\n'
     stream += ')))\n'
     stream += '\t(return-from state' + state_num + ' 0))\n\n'
 
     return stream
+
 
 def generate_states():
     stream = ''
@@ -138,6 +132,7 @@ def generate_states():
         stream += generate_state(state)
 
     return stream
+
 
 def generate_check_alphabet():
     stream = ''
@@ -148,30 +143,32 @@ def generate_check_alphabet():
     stream += '\t\t\t(return-from checkalphabet 0))))\n\n'
     return stream
 
+
 def generate_demo():
     start_state_number = tokens[3]
     stream = ''
-    stream += '(defun demo () "runs the fsa processing\n"'
-    stream += '\t(setq file (open filename :direction :input))'
-    stream += '\t(setq strlist (read file "done"))'
-    stream += '\t(princ "processing ")'
-    stream += '\t(terpri)'
-    stream += '\t(setq current-state 0)'
-    stream += '\t(setq accept 0)'
+    stream += '(defun demo ()\n'
+    stream += '\t(setq file (open filename :direction :input))\n'
+    stream += '\t(setq strlist (read file "done"))\n'
+    stream += '\t(princ "processing ")\n'
+    stream += '\t(terpri)\n'
+    stream += '\t(setq current-state 0)\n'
+    stream += '\t(setq accept 0)\n'
     stream += '\t(setq alphabet \'('
     # add the alphabet
     for a in alphabet:
         stream += a + ' '
     # cut the last space character out
-    stream = stream[:len(stream)-1]
-    stream += '))'
-    stream += '\t(setq inalphabet (checkalphabet strlist alphabet))'
-    stream += '\t(setq success (state' + start_state_number + ' strlist))'
-    stream += '\t(if (and (= success 1) (= inalphabet 1))'
-    stream += '\t\t(write "string is legal")'
-    stream += '\t\t(write "string is illegal")))'
+    stream = stream[:len(stream) - 1]
+    stream += '))\n'
+    stream += '\t(setq inalphabet (checkalphabet strlist alphabet))\n'
+    stream += '\t(setq success (state' + start_state_number + ' strlist))\n'
+    stream += '\t(if (and (= success 1) (= inalphabet 1))\n'
+    stream += '\t\t(write "string is legal")\n'
+    stream += '\t\t(write "string is illegal")))\n'
 
     return stream
+
 
 def generate_lisp_code():
     global_var = '(defvar filename "theString.txt")\n\n'
@@ -183,19 +180,8 @@ def generate_lisp_code():
 
     output_file = open(output_filename, "w")
 
-
-# def check_valid_string():
-#     start_state_number = tokens[3]
-#
-#     start_state = fsa.search_state(state_nodes, start_state_number)
-#     end_state = fsa.traverse_fsa(start_state, transition_nodes, input_string)
-#
-#     # end_state.print()
-#
-#     if end_state.is_accept_state():
-#         print("input string is legal for given FSA")
-#     else:
-#         print("input string is illegal for given FSA")
+    output_file.write(lisp_stream)
+    output_file.close()
 
 
 parse_fsa()
